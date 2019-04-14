c956ad071e01d1bb8fd7274150a02222cfaf4821
Della Vedova Maria Luisa <IT244@itad.infotn.it>
2019-04-14 14:57:19 +0200

    set local repo

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  title = "mldv Angular App nuova nuova prova";
+  title = "mldv Angular App nuova nuova prova git";
   
   constructor() { }
 
